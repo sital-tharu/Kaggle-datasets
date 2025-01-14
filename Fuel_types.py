5c888caec27f86831511267ed63eb453fed5840e
sital-tharu <sitatharu769@gmail.com>
2025-01-14 17:25:56 +0530

    i dentify the types of fuel types

--- Fuel_types.py
+++ Fuel_types.py
@@ -1,6 +1,9 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
+# Loading dataset 
 data = pd.read_csv("cardata.csv")
 
-print(data.head())
+# Count the occurrences of each fue; type
+fuel_counts = data["Fuel_Type"].value_counts
+print(fuel_counts)
