dc58ddba169c3f784aeb11a3cef24f1cd124abcb
sital-tharu <sitatharu769@gmail.com>
2025-01-14 17:48:55 +0530

    adding color to bar

--- Fuel_types.py
+++ Fuel_types.py
@@ -9,7 +9,7 @@ fuel_counts = data["Fuel_Type"].value_counts()
 
 #ploting the data
 plt.figure(figsize=(10,6))
-fuel_counts.plot(kind="bar")
+fuel_counts.plot(kind="bar", color=["blue","green", "red"])
 plt.xlabel("FUEL TYPE")
 plt.ylabel("NUMBER OF CARS")
 plt.title("DISTRIBUTUION OF DIFFERENT FUEL TYPES")
